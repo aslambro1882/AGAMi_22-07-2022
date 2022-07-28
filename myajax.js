20557a200a0f9d51f776abba92425f113b0ca3ac
Maruf Bin Solaiman <0909maruf@gmail.com>
2022-07-28 18:13:12 +0600

    push

--- myajax.js
+++ myajax.js
@@ -1,117 +1,155 @@
 $(document).ready(function () {
-  function showSplits() {
-    $.ajax({
-      url: "splits.json",
-      method: "GET",
-      dataType: "json",
-      success: function (data) {
-        let html;
-        const styles = ["Saab", "Volvo", "BMW"];
-        html += "<th>  </th>";
-
-        for (i = 0; i < data.length; i++) {
-          html += "<th class='text-center'>" + data[i].splittitle + "</th>";
-          //console.log(data[i].id);
-        }
-        html += "<th>CGPA</th>";
-        $("#split").html(html);
-      },
+  // Ajax call for grades ------------
+  var gradesArray;
+  function getGrades() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "/grades.json",
+        method: "GET",
+        dataType: "json",
+        success: function (grades) {
+          gradesArray = grades;
+          resolve();
+        },
+        error: () => reject(),
+      });
     });
   }
 
-  showSplits();
+  // Ajax call for Splits ------------
+  var splitsArray;
+  function getSplits() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "/splits.json",
+        async: false,
+        method: "GET",
+        dataType: "json",
+        success: function (splits) {
+          splitsArray = splits;
+          resolve();
+        },
+        error: () => reject(),
+      });
+    });
+  }
 
-  // function showDpt() {
-  //     $.ajax({
-  //         url: "courses.json",
-  //         method: "GET",
-  //         dataType: "json",
-  //         success: function (data) {
+  // Ajax call for courses ------------
+  var coursesArray;
+  function getCourses() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "/courses.json",
+        async: false,
+        method: "GET",
+        dataType: "json",
+        success: function (courses) {
+          coursesArray = courses;
+          resolve();
+        },
+        error: () => reject(),
+      });
+    });
+  }
 
-  //             let html;
+  // Ajax call for students ------------
+  var studentObj;
+  function getStudent() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: "/student.json",
+        async: false,
+        method: "GET",
+        dataType: "json",
+        success: function (student) {
+          studentObj = student;
+          resolve();
+        },
+        error: () => reject(),
+      });
+    });
+  }
 
-  //             for (i = 0; i < data.length; i++) {
-  //                 html += "<tr>"
-  //                     + "<td class='table-primary'>" + data[i].coursetitle + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>"
-  //                     + "<td>" + "</td>" +
-  //                     "</tr>";
-  //                 //console.log(data[i].id);
-  //             }
-  //             $("#dpt").html(html);
+  let gradePromise = getGrades();
+  gradePromise
+    .then((result) => {
+      let splitPromise = getSplits();
+      splitPromise
+        .then((result) => {
+          let coursePromise = getCourses();
+          coursePromise
+            .then((result) => {
+              let studentPromise = getStudent();
+              studentPromise
+                .then((result) => {
+                  // showTableHeaders();
+                  showTableData();
+                })
+                .catch((err) => {});
+            })
+            .catch((err) => {});
+        })
+        .catch((err) => {});
+    })
+    .catch((err) => {});
 
-  //         }
-  //     });
+  console.log("grades", gradesArray);
+  console.log("splits", splitsArray);
+  console.log("courses", coursesArray);
+  console.log("student", studentObj);
+
+  // function getGP(mark, grade) {
+  //   if (mark >= 0 && mark <= 100) {
+  //     if (mark < grade.upper && mark >= grade.lower) {
+  //       return parseFloat(grade.gradepoint);
+  //     }
+  //   } else {
+  //     return "-";
+  //   }
   // }
 
-  // showDpt();
+  function showTableData() {
+    let htmlH;
+    htmlH += "<th>  </th>";
+    for (i = 0; i < splitsArray.length; i++) {
+      htmlH += `<th class='text-center'>${splitsArray[i].splittitle}</th>`;
+      //console.log(data[i].id);
+    }
+    htmlH += "<th>GP</th>";
+    $("#split").html(htmlH);
 
-  function showDpt() {
-    $.ajax({
-      url: "courses.json",
-      method: "GET",
-      dataType: "json",
-      success: function (courses) {
-        $.ajax({
-          url: "splits.json",
-          method: "GET",
-          dataType: "json",
-          success: function (splits) {
-            $.ajax({
-              url: "student.json",
-              method: "GET",
-              dataType: "json",
-              success: function (student) {
-                let html;
-                for (i = 0; i < courses.length; i++) {
-                  html += `<tr><td class='fw-bold'>${courses[i].coursetitle}</td>`;
-                  let m;
-                  for (j = 0; j < splits.length; j++) {
-                    for (k = 0; k < student.marks.length; k++) {
-                      console.log(student.marks[k].splitno);
-                      if (
-                        courses[i].coursecode == student.marks[k].coursecode &&
-                        splits[j].splitno == student.marks[k].splitno
-                      ) {
-                        m = student.marks[k].marks;
-                        if (m == null) m = "-";
-                        html += `<td class='text-center'>${m}</td>`;
-                        break;
-                      }
-                    }
-                  }
-                  //   $.ajax({
-                  //     url: "grades.json",
-                  //     method: "GET",
-                  //     dataType: "json",
-                  //     success: function (grades) {
-                  //       console.log(m);
-                  //       //   for (let grade of grades) {
-                  //       //     console.log(m);
-                  //       //     console.log(grade.upper, m, grade.lower);
-                  //       //     if (m < grade.upper && m >= grade.lower) {
-                  //       //       html += `<td class='text-center'>${grade.gradepoint}</td></tr>`;
-                  //       //     }
-                  //       //   }
-                  //     },
-                  //   });
-                  html += "<td class='text-center'> 4 </td></tr>";
-                }
-                $("#dpt").html(html);
-              },
-            });
-          },
-        });
-      },
-    });
+    let html;
+    for (i = 0; i < coursesArray.length; i++) {
+      html += `<tr><td class='fw-bold'>${coursesArray[i].coursetitle}</td>`;
+      let m;
+      for (j = 0; j < splitsArray.length; j++) {
+        for (k = 0; k < studentObj.marks.length; k++) {
+          // console.log(student.marks[k].splitno);
+          if (
+            coursesArray[i].coursecode == studentObj.marks[k].coursecode &&
+            splitsArray[j].splitno == studentObj.marks[k].splitno
+          ) {
+            m = studentObj.marks[k].marks;
+            if (m == null) m = "-";
+            html += `<td class='text-center'>${m}</td>`;
+            break;
+          }
+        }
+      }
+      let gp;
+      for (let grade of gradesArray) {
+        gp = getGP(m, grade);
+        console.log(gp);
+        if (mark >= 0 && mark <= 100) {
+          if (mark < grade.upper && mark >= grade.lower) {
+            gp = parseFloat(grade.gradepoint);
+          }
+        } else {
+          gp = "-";
+        }
+      }
+      html += `<td class='text-center'>${gp}</td></tr>`;
+    }
+    $("#dpt").html(html);
   }
-  showDpt();
 });
